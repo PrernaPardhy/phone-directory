31728b1bbce20b2f0fbaab3d700972b18d357481
PrernaPardhy <prerna.pardhy@gmailcom>
2019-01-18 13:51:51 +0530

    Created Skeleton of Home Pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+        {/* <div className="header">
+          phone directory
+        </div>
+        <button>Add</button><br />
+        <span>Name</span><br />
+        <span>Phone</span> */}
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue="Prerna"/>
       </div>
     );
   }
